9b04481c0fe9066d711e09c7ad121546407dcad3
Nathan Gillespie <crookedtwig4@gmail.com>
2024-12-16 23:03:52 -0800

    Now Expandable

---
+++ JS/doNotOpen.js
@@ -0,0 +1,8 @@
+function getSet(code) {
+    document.getElementById('Set1').style.display = "none";
+    switch (code) {
+        case "Camden":
+            document.getElementById('Set1').style.display = "inline";
+            break;
+    }
+}
